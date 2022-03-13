d3ce32ab1f07cc6e4b4fafb1aedf158cfe21fb71
Kamil <kchrobok.contact@gmail.com>
2022-03-13 12:07:42 +0100

    Add app configurations file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,25 @@ import { AppComponent } from "./app.component";
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
+import { APP_INITIALIZER } from '@angular/core';
+import { AppConfigService } from "./app-config.service";
+import { HttpClientModule } from "@angular/common/http";
+
 @NgModule({
   declarations: [AppComponent, NavbarComponent, HomepageComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [
+      {
+          provide: APP_INITIALIZER,
+          multi: true,
+          deps: [AppConfigService],
+          useFactory: (appConfigService: AppConfigService) => {
+              return () => {
+                  return appConfigService.loadAppConfig();
+              }
+          }
+      }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

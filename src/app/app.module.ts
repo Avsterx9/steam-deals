9318a733ba916a5d25a729ecbd30601a0ce12265
Mateusz Żebrak <mateusz.zebrak@gmail.com>
2022-03-13 14:57:12 +0100

    Run prettier with new settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import {APP_INITIALIZER, NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { HomepageComponent } from "./homepage/homepage.component";
-
-import { APP_INITIALIZER } from "@angular/core";
-import { AppConfigService } from "./app-config.service";
-import { HttpClientModule } from "@angular/common/http";
-import { FooterComponent } from "./footer/footer.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {HomepageComponent} from "./homepage/homepage.component";
+import {AppConfigService} from "./app-config.service";
+import {HttpClientModule} from "@angular/common/http";
+import {FooterComponent} from "./footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomepageComponent,
-    FooterComponent,
-  ],
+  declarations: [AppComponent, NavbarComponent, HomepageComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [
     {

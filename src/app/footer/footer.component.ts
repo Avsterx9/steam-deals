75641a4fd52bbe0f2422ec07b428ff37dbfa17bf
Rafał Żyła <rafal.zyla99@gmail.com>
2022-03-15 12:06:28 +0100

    Add environments with specified environment configuration

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import {HttpClient} from "@angular/common/http";
 import {Component} from "@angular/core";
-import {AppConfigService} from "../app-config.service";
+import {environment} from "src/environments/environment";
 
 @Component({
   selector: "app-footer",
@@ -10,12 +10,12 @@ import {AppConfigService} from "../app-config.service";
 export class FooterComponent {
   apiVersion!: string;
 
-  constructor(private configService: AppConfigService, private http: HttpClient) {
+  constructor(private http: HttpClient) {
     this.getCurrentApiVersion();
   }
 
   private getCurrentApiVersion() {
-    this.http.get(this.configService.apiBaseUrl).subscribe((response: any) => {
+    this.http.get(environment.backendURL).subscribe((response: any) => {
       this.apiVersion = response.version;
     });
   }
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  backendURL: "http://nwta.eastus.cloudapp.azure.com:5555",
+};

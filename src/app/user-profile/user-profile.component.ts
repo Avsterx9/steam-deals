87ed25f853832dae04e91be2a3fe5d6e75da679b
Kamil <kchrobok.contact@gmail.com>
2022-04-12 11:41:31 +0200

    Add redirecting to /login when user is not logged in

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,21 @@
 import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {UserAuthenticationService} from "../services/user-authentication.service";
 
 @Component({
   selector: "app-user-profile",
   templateUrl: "./user-profile.component.html",
   styleUrls: ["./user-profile.component.sass"],
 })
-export class UserProfileComponent {
-  constructor() {}
+export class UserProfileComponent implements OnInit {
+  constructor(private authenticationService: UserAuthenticationService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.authenticationService.getUserDetails().subscribe(
+      (res: any) => {},
+      (err) => {
+        this.router.navigate(["/login"]);
+      }
+    );
+  }
 }

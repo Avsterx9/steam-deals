8e63d7974af1105a02d31bcc1ae3c1a8ffdb033b
Kamil <kchrobok.contact@gmail.com>
2022-04-19 13:04:45 +0200

    Update authentication tests with use of commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,43 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const {faker} = require("@faker-js/faker");
+
+function createRandomUSer() {
+    const uniqueFirstName = faker.unique(faker.name.firstName);
+    const uniqueLastName = faker.unique(faker.name.lastName);
+    const userName = `${uniqueFirstName}_${uniqueLastName}_${generate_randomID()}`.toLowerCase();
+
+    return {
+        'userName': userName,
+        'firstName': uniqueFirstName,
+        'lastName': uniqueLastName,
+        'email': `${userName}@gmail.com`,
+        'password': `${faker.internet.password()}`
+    };
+}
+
+function generate_randomID() {
+    return faker.datatype.uuid().split('-')[0];
+}
+
+Cypress.Commands.add('register', (user) => {
+    cy.visit('/');
+    cy.contains("button", "Log in").click();
+    cy.contains("a", "Sign up").click();
+    cy.get("input[name='userName']").type(user.userName);
+    cy.get("input[name='firstName']").type(user.firstName);
+    cy.get("input[name='lastName']").type(user.lastName);
+    cy.get("input[name='email']").type(user.email);
+    cy.get("input[name='password1']").type(user.password);
+    cy.get("input[name='password2']").type(user.password);
+    cy.contains("button", "Sign up").click();
+});
+
+Cypress.Commands.add('login', (user) => {
+    cy.visit('/');
+    cy.contains("button", "Log in").click();
+    cy.get("input[name='username']").type(user.userName);
+    cy.get("input[name='password']").type(user.password);
+    cy.contains("button[class='submit-button']", "Log in").click();
+    cy.contains("div[class='logged-user-panel']", user.userName);
+});
+
+Cypress.Commands.add('createRandomUSer', createRandomUSer);

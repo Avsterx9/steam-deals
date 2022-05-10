950f4bad66653bf0acfb0e00ee1b09149076562b
Kamil <kchrobok.contact@gmail.com>
2022-05-10 16:03:55 +0200

    Add function to abbreviate number of game owners

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -88,6 +88,19 @@ export class HomepageComponent {
       return;
     }
     this.getTopGames();
-    return;
+  }
+
+  abbreviateNumber(num: number) {
+    let SI_SYMBOL = ["", "k", "M", "G", "T", "P", "E"];
+
+    let tier = (Math.log10(Math.abs(num)) / 3) | 0;
+    if (tier == 0) return num;
+
+    let suffix = SI_SYMBOL[tier];
+    let scale = Math.pow(10, tier * 3);
+
+    let scaled = num / scale;
+
+    return scaled.toFixed(1) + suffix;
   }
 }

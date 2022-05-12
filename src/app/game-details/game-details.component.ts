fe9b35aee468ee66535fc364c3b82a7d59a44fa2
Rafał Żyła <rafal.zyla99@gmail.com>
2022-05-12 15:03:11 +0200

    Display basic information in game-details-component

--- src/app/game-details/game-details.component.ts
+++ src/app/game-details/game-details.component.ts
@@ -1,10 +1,25 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {GamesService} from "../services/games.service";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {Observable, switchMap} from "rxjs";
+import {IGame} from "../game-interfaces/game";
+import {Location} from "@angular/common";
 
 @Component({
   selector: "app-game-details",
   templateUrl: "./game-details.component.html",
   styleUrls: ["./game-details.component.sass"],
 })
-export class GameDetailsComponent {
-  constructor() {}
+export class GameDetailsComponent implements OnInit {
+  gameDetails!: Observable<IGame>;
+
+  constructor(private http: GamesService, private route: ActivatedRoute, private location: Location) {}
+
+  ngOnInit() {
+    this.gameDetails = this.route.paramMap.pipe(switchMap((params: ParamMap) => this.http.getGame(params.get("id")!)));
+  }
+
+  goToGames() {
+    this.location.back();
+  }
 }
--- src/app/services/games.service.ts
+++ src/app/services/games.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {IGame} from "../game-interfaces/game";
+import {Observable} from "rxjs";
 @Injectable({
   providedIn: "root",
 })
@@ -20,5 +21,10 @@ export class GamesService {
     let params = new HttpParams().set("skip", skipAmount).set("amount", amount);
 
     return this.http.get(this.backendURL + "/top100in2weeks", {withCredentials: true, params});
+  }
+
+  public getGame(id: string): Observable<IGame> {
+    let params = new HttpParams().set("app_id", id);
+    return this.http.get<IGame>(this.backendURL + "/id/{appid}", {withCredentials: true, params});
   }
 }

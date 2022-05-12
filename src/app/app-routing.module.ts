ba9c1fc7c8dd98e565b5bb7e3cb0a7af0c5a1cf8
Rafał Żyła <rafal.zyla99@gmail.com>
2022-05-12 13:13:15 +0200

    Add routing path to game-details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {HomepageComponent} from "./homepage/homepage.component";
 import {LoginComponent} from "./login/login.component";
 import {RegistrationComponent} from "./registration/registration.component";
 import {UserProfileComponent} from "./user-profile/user-profile.component";
+import {GameDetailsComponent} from "./game-details/game-details.component";
 
 const routes: Routes = [
   {path: "", component: HomepageComponent},
   {path: "login", component: LoginComponent},
   {path: "registration", component: RegistrationComponent},
   {path: "user", component: UserProfileComponent},
+  {path: "game/:id", component: GameDetailsComponent},
 ];
 
 @NgModule({
